c086f5d3eeb814e9168060b686af5edd4fd1ed52
kivetat-tatevik <kivetat.85@yandex.ru>
2023-10-13 09:32:15 +0200

    Initial Commit

---
+++ RGB/RGBApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBApp.swift
+//  RGB
+//
+//  Created by Tatevik Khunoyan on 13.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct RGBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
